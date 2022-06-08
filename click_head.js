25429229827be9047332842c0241669053ba5cf0
PieroMFAL <u201610857@upc.edu.pe>
2022-06-08 16:06:36 +0200

    feat:tb4 commit

--- click_head.js
+++ click_head.js
@@ -3,6 +3,12 @@ var move_up = document.querySelectorAll('.move_up')
 var click_head = document.querySelector('.head-click');
 //console.log(move_up);
 
+const flagsElement = document.getElementById("flags");
+
+flagsElement.addEventListener("click",(e)=>{
+    console.log(e.target.parenElement.dataset.language);
+});
+
 button.addEventListener('click', () =>{
     click_head.classList.toggle('aparecer')
     console.log(move_up)
