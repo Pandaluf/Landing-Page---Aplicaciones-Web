d54c057f71859a8c0dc98a8199e938fb59c17187
MiguelGarcia <u202011292@upc.edu.pe>
2022-06-27 07:50:18 -0500

    feat: Final version to Landing Page with the option to change language

--- click_head.js
+++ click_head.js
@@ -1,12 +1,26 @@
 var button = document.querySelector('.menu');
-var move_up = document.querySelectorAll('.move_up')
+var move_up = document.querySelectorAll('.move_up');
 var click_head = document.querySelector('.head-click');
 //console.log(move_up);
 
 const flagsElement = document.getElementById("flags");
 
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) => {
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+
+    for(const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        textToChange.innerHTML = texts[section][value];
+    }
+};
+
 flagsElement.addEventListener("click",(e)=>{
-    console.log(e.target.parenElement.dataset.language);
+    changeLanguage(e.target.parentElement.dataset.language);
 });
 
 button.addEventListener('click', () =>{
